d64b3db469e752a98ac43867537bcaf4e6745bf1
tunangoo <ngovantuan02092003@gmail.com>
2024-04-30 00:34:08 +0700

    update userservice

---
+++ src/main/java/com/example/viettravel_backend/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.viettravel_backend.service;
+
+import com.example.viettravel_backend.dto.request.AddFavoriteRequest;
+import com.example.viettravel_backend.exception.ParamInvalidException;
+import com.example.viettravel_backend.repository.PlaceRepository;
+import com.example.viettravel_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
